272fdc8adb5c1264d91aa1d3ee646ca0eda616d5
Jay-Dee <johnsondesouza@gmail.com>
2023-11-28 17:30:21 +0000

    Add project files.

---
+++ Codility/BinaryGap.cs
@@ -0,0 +1,39 @@
+﻿namespace BinaryGap {
+    internal class Solution {
+        public int solution(int N) {
+            int largestBinaryGap = 0;
+            string numberAsBinaryString = Convert.ToString(N, 2);
+            Console.WriteLine($"{N} = {numberAsBinaryString}");
+            int foundOne = 0;
+            for(int counter = 0; counter < numberAsBinaryString.Length; counter++) {
+                int binaryGap = 0;
+                if (numberAsBinaryString[counter] == '1') {
+                    foundOne++;
+                    for(int internalCounter  = counter + 1; internalCounter < numberAsBinaryString.Length; internalCounter++) {
+                        if (numberAsBinaryString[internalCounter] == '0') {
+                            if(internalCounter + 1 == numberAsBinaryString.Length) {
+                                binaryGap = 0;
+                            } else {
+                                binaryGap++;
+                            }
+                        } else {
+                            counter = internalCounter - 1;
+                            break;
+                        }
+                    }
+                }
+
+                if(largestBinaryGap < binaryGap) {
+                    largestBinaryGap = binaryGap;
+                }
+            }
+
+            if (foundOne <= 1) {
+                largestBinaryGap = 0;
+            }
+
+            Console.WriteLine($"LargestBinaryGap = {largestBinaryGap}");
+            return largestBinaryGap;
+        }
+    }
+}
---
+++ Codility/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Codility {
+    internal class Program {
+        static void Main(string[] args) {
+            int[] binaryGapTests = new int[] {0, 1, 2, 9, 15, 20, 32, 529, 1041 };
+            foreach (int test in binaryGapTests) {
+                var sln = new BinaryGap.Solution();
+                sln.solution(test);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
