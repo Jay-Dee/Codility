541dd5dbd6f78cc502c7c4bbac666951bc70f3b2
Jay-Dee <johnsondesouza@gmail.com>
2023-11-29 12:08:32 +0000

    Cyclic rotation implementation

---
+++ Codility/CyclicRotation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyclicRotation {
+    internal class Solution {
+        public int[] solution(int[] A, int K) {
+            Console.WriteLine($"{string.Join(", ", A)} rotated {K} times");
+            var srcArray = new int[A.Length];
+            var rotatedArray = new int[A.Length];
+            A.CopyTo(rotatedArray, 0);
+            if(K %  A.Length != 0) {
+                for(int rotationCounter = 1; rotationCounter <= K ; rotationCounter++) {
+                    rotatedArray.CopyTo(srcArray, 0);
+                    for(int elementCounter = 0; elementCounter < A.Length; elementCounter++) {
+                        int srcElementCounter = elementCounter + 1;
+                        if(srcElementCounter > A.Length - 1) {
+                            srcElementCounter = srcElementCounter - A.Length;
+                        }
+
+                        if(srcElementCounter == 0) {
+                            rotatedArray[srcElementCounter] = srcArray[srcArray.Length - 1];
+                        } else {
+                            rotatedArray[srcElementCounter] = srcArray[srcElementCounter - 1];
+                        }
+                    }
+                    Console.WriteLine(string.Join(", ", rotatedArray));
+                }
+            }
+            Console.WriteLine($"Final rotation : {string.Join(", ", rotatedArray)}");
+            return rotatedArray;
+        }
+    }
+}
--- Codility/Program.cs
+++ Codility/Program.cs
@@ -1,12 +1,17 @@
 ﻿namespace Codility {
     internal class Program {
         static void Main(string[] args) {
-            int[] binaryGapTests = new int[] {0, 1, 2, 9, 15, 20, 32, 529, 1041 };
-            foreach (int test in binaryGapTests) {
-                var sln = new BinaryGap.Solution();
-                sln.solution(test);
-            }
-            
+            //int[] binaryGapTests = new int[] {0, 1, 2, 9, 15, 20, 32, 529, 1041 };
+            //foreach (int test in binaryGapTests) {
+            //    var sln = new BinaryGap.Solution();
+            //    sln.solution(test);
+            //}
+
+            int[] sourceArray = { 3, 8, 9, 7, 6 };
+            var sln = new CyclicRotation.Solution();
+            sln.solution(sourceArray, 3);
+
+
             Console.ReadKey();
         }
     }
